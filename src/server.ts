b94fb05d3ec52f7e2ec6033af958ef3a94cba4ce
Hussainforvercel <iqradaud643@gmail.com>
2024-10-18 13:08:53 -0700

    first commit

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+import connectDB from "./config/db";
+import authRoutes from "./routes/authRoutes";
+import reviewRoutes from "./routes/reviewRoutes";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+connectDB();
+const app: Application = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/users", authRoutes);
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World!");
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
