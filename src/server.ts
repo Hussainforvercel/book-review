8b81753f4e290221336e73faa68ccbe9e063f6bf
Hussainforvercel <iqradaud643@gmail.com>
2024-10-19 05:29:22 -0700

    origin is defined

--- src/server.ts
+++ src/server.ts
@@ -14,11 +14,7 @@ if (!process.env.PORT) {
 }
 const PORT = parseInt(process.env.PORT, 10);
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://book-review-app-rouge.vercel.app",
-  "https://book-review-murex.vercel.app",
-];
+const allowedOrigins = ["https://book-review-app-rouge.vercel.app"];
 
 const corsOptions = {
   origin: function (origin: string | undefined, callback: Function) {
